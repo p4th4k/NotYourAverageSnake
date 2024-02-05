2f8e992a8a7b580f780570de3f067533e48e3977
p4th4k <75157529+C4TT0@users.noreply.github.com>
2024-02-05 18:42:54 +0530

    Added food, food collision detection, randomize its spawning, checking game over

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ class Box{
     constructor(x, y){
         this.x = x;
         this.y = y;
+        this.size = 20;
     }
 }
 
@@ -20,7 +21,6 @@ class Snake extends Box{
         this.velY = 7;
         this.isX = false;
         this.isY = false;
-        this.size = 20;
         this.color = "#6fdb49";
         this.score = 0;
         this.startingLength = 3;
@@ -60,16 +60,61 @@ class Snake extends Box{
         if (this.isX) this.x += this.velX;
         if (this.isY) this.y += this.velY;
     }
+
+    checkGameOver(){
+        if(this.x <= 0){
+            alert("Game Over")
+        }
+        if((this.x + this.size) >= width){
+            alert("Game Over");
+        }
+        if(this.y <= 0){
+            alert("Game Over");
+        }
+        if((this.y + this.size) >= height){
+            alert("Game Over");
+        }
+    }
+
+    detectFoodCollison(){
+        if(
+            this.x < food.x + food.size && 
+            food.x < this.x + this.size &&
+            this.y < food.y + food.size &&
+            food.y < this.y + this.size
+        ){
+            food.x = random(100, width - 100);
+            food.y = random(100, height - 100);
+        }
+    }
+}
+
+class Food extends Box{
+    constructor(x, y){
+        super(x, y);
+        this.color = "#eb3534";
+    }
+
+    draw(){
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.size, this.size)
+    }
 }
 
 const snake = new Snake(random(100, width - 100), random(100, height - 100));
+const food = new Food(random(100, width - 100), random(100, height - 100));
 
 const gameLoop = () => {
     ctx.fillStyle = "#0b0f1a";
     ctx.fillRect(0, 0, width, height);
 
+    food.draw();
+
     snake.drawSnakeHead();
+    snake.checkGameOver();
     snake.update();
+    snake.detectFoodCollison();
+
 
     requestAnimationFrame(gameLoop);
 }
